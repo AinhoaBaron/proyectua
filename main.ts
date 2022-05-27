62e92d1e6bd7216f6b06851a84f86c815dde7852
AinhoaBaron <93262452+AinhoaBaron@users.noreply.github.com>
2022-05-27 14:26:05 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,46 @@
 let distantzia = 0
-let _1_atala = 0
-_1_atala = 1
 DFRobotMaqueenPlus.I2CInit()
+let _1_atala = 1
+let _2_atala = 0
+let _3_atala = 0
 basic.forever(function () {
-    distantzia = DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2)
+    distantzia = 0
 })
 basic.forever(function () {
-    _1_atala = 1
-    if (DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrolVoltage(Patrol.R1) == 1) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
-    } else {
-        if (DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrolVoltage(Patrol.R1) == 0) {
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 140)
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 30)
-        } else {
-            if (DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrolVoltage(Patrol.R1) == 1) {
-                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 140)
-                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
-            }
+    if (_1_atala == 1) {
+        if (distantzia > 60) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 140)
+        } else if (distantzia > 50) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 110)
+        } else if (distantzia > 40) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 80)
+        } else if (distantzia > 30) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+        } else if (distantzia > 20) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 20)
+        } else if (distantzia > 10) {
+            DFRobotMaqueenPlus.mototStop(Motors.ALL)
+            _1_atala = 0
+            _2_atala = 1
         }
     }
 })
 basic.forever(function () {
-    _1_atala = 0
-    if (distantzia > 60) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 140)
-    } else if (distantzia > 50) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 110)
-    } else if (distantzia > 40) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 80)
-    } else if (distantzia > 30) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
-    } else if (distantzia > 20) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 20)
+    if (_2_atala == 1) {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+        } else {
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 140)
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+            } else {
+                if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+                    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 140)
+                    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+                }
+            }
+        }
     }
-    DFRobotMaqueenPlus.mototStop(Motors.M1)
-    _1_atala += 1
+    _2_atala = 0
+    _3_atala = 1
 })
